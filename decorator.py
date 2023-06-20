5195115ca232b91a199f6454b8a7012f91ca8e1d
raghava <raghavayarra@gmail.com>
2023-06-20 11:42:02 +0530

    first commit

---
+++ decorator.py
@@ -0,0 +1,34 @@
+'''def my_func(func):
+    def inner(name):
+        if name == "sunny":
+            print("hello sunny badmrng!!!")
+        else:
+            func(name)
+    return inner
+#@my_func
+def wish(name):
+    print("hello",name,"gudmrng!!!")
+
+wish("raghava")
+wish("sunny")
+dec=my_func(wish)
+dec("raghava")
+dec("sunny")
+
+'''
+
+def smart_division(func):
+    def inner(a,b):
+        if b==0:
+            print("we can;t divide if b is zero")
+            return
+        else:
+            func(a,b)
+    return inner
+@smart_division
+
+def division(a,b):
+    return a/b
+
+s=division(20,10)
+print(s)
