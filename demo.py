de11984f77242d0e20fe0680ace084556e8cf080
raghava <raghavayarra@gmail.com>
2023-06-20 14:06:22 +0530

    adding demo file

---
+++ demo.py
@@ -0,0 +1,69 @@
+def my_func(func):
+    def inner(name):
+        if name == "sunny":
+            print("hello sunny badmrng!!!")
+        else:
+            func(name)
+    return inner
+#@my_func
+def wish(name):
+    print("hello",name,"gudmrng!!!")
+
+wish("raghava")
+wish("sunny")
+dec=my_func(wish)
+dec("raghava")
+dec("sunny")
+dec("kiarn")
+
+
+def smart_division(func):
+    def inner(a,b):
+        if b==0:
+            print("we can;t divide if b is zero")
+            return
+        else:
+            func(a,b)
+    return inner
+@smart_division
+
+def division(a,b):
+    return a/b
+
+s=division(20,10)
+print(s)
+
+s=division(20,0)
+print(s)
+
+
+def wish(name):
+    print("hi",name,"gudmrng!!")
+
+wish("raghava")
+wish("kiran")
+
+
+def my_func(func):
+    def inner(name):
+        if name == "sunny":
+            print("hello sunny badmrng!!!")
+        else:
+            func(name)
+    return inner
+#@my_func
+def wish(name):
+    print("hello",name,"gudmrng!!!")
+
+wish("raghava")
+wish("sunny")
+dec=my_func(wish)
+dec("raghava")
+dec("sunny")
+dec("kiarn")
+
+
+def square(n):
+    return n*n
+
+square(5)
